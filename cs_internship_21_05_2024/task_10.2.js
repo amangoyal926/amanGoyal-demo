0d5250eedac7c6c3f6401d47a29a852f82557f99
Aman Goyal <amangoyal926@gmail.com>
2024-05-22 17:12:48 +0530

    Update task_10.2.js

--- cs_internship_21_05_2024/task_10.2.js
+++ cs_internship_21_05_2024/task_10.2.js
@@ -2,14 +2,15 @@ const prompt = require("prompt-sync")({sigint:true});
 let n=prompt("Enter any number");
 n=Number.parseInt(n);
 for(let i=0;i<n;i++){
+    let str="";
     for(let j=0;j<n-i-1;j++){
-        process.stdout.write(" ");
+        str+=" ";
     }
     for(let j=0;j<2*i+1;j++){
-        process.stdout.write(j+1);
+        str+=j+1;
     }
     for(let j=0;j<n-i-1;j++){
-        process.stdout.write(" ");
+        str+=" ";
     }
-    console.log();
+    console.log(str);
 }
